677f8b32c8299658c6c46c58a38bebb24ee894de
hug.m <hug.m@foxmail.com>
2019-07-10 19:14:38 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Hug.m</a> 设计和编码 <span style="color: #e27575; font-size: 14px;">💗</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>

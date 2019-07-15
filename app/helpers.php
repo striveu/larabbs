c8108f1e448aaefcc89a6bd87bc3fc4a5278a69f
hug.m <hug.m@foxmail.com>
2019-07-15 23:19:43 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,10 @@ if (!function_exists('route_class')) {
         return str_replace('.', '-', Route::currentRouteName());
     }
 }
+
+if (!function_exists('category_nav_active')) {
+    function category_nav_active($category_id)
+    {
+        return active_class(if_route('categories.show') && if_route_param('category', $category_id));
+    }
+}

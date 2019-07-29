3ebf0c597c14fd12ad4c4984bf4d7fe4747611bf
hug.m <hug.m@foxmail.com>
2019-07-30 00:01:13 +0800

    集成 IDE Helper

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,6 +2,47 @@
 
 namespace App\Models;
 
+/**
+ * App\Models\Topic
+ *
+ * @property int $id
+ * @property string $title 帖子标题
+ * @property string $body 帖子内容
+ * @property int $user_id 用户 ID
+ * @property int $category_id 分类 ID
+ * @property int $reply_count 回复数量
+ * @property int $view_count 查看总数
+ * @property int $last_reply_user_id 最后回复的用户 ID
+ * @property int $order 可用来做排序使用
+ * @property string|null $excerpt 文章摘要，SEO 优化时使用
+ * @property string|null $slug SEO 友好的 URL
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category $category
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Reply[] $replies
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic recent()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic recentReplied()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereExcerpt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereLastReplyUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereReplyCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereViewCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic withOrder($order)
+ * @mixin \Eloquent
+ */
 class Topic extends Model
 {
     protected $fillable = [
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,43 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $avatar 头像
+ * @property string|null $introduction 个人简介
+ * @property int $notification_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Reply[] $replies
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Role[] $roles
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Topic[] $topics
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereIntroduction($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereNotificationCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use Traits\ActiveUserHelper;
